5320c2fbab666ff83ac567468dd15510c9d1d7c2
wangweiye <wwyknight@163.com>
2021-01-17 20:37:43 +0800

    fix

--- provider-service/src/main/java/cc/wangweiye/providerservice/HelloController.java
+++ provider-service/src/main/java/cc/wangweiye/providerservice/HelloController.java
@@ -1,14 +1,18 @@
 package cc.wangweiye.providerservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${server.port}")
+    private int port;
+
     @RequestMapping("/hello")
     public String hello(String name) throws InterruptedException {
-        Thread.sleep(60 * 1000);
+        //Thread.sleep(60 * 1000);
 
-        return "hello " + name;
+        return port + ": hello " + name;
     }
 }
