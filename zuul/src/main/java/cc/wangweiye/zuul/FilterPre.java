0aabe84460c25c26727f5c8288179a3078fa20b2
wangweiye <wwyknight@163.com>
2021-01-19 09:24:24 +0800

    fix

---
+++ zuul/src/main/java/cc/wangweiye/zuul/FilterPre.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2015-现在 广州市领课网络科技有限公司
+ */
+package cc.wangweiye.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+/**
+ * 请求开始前执行
+ */
+public class FilterPre extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(FilterPre.class);
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        String uri = RequestContext.getCurrentContext().getRequest().getServletPath();
+
+        if (uri.contains("/consumer")) {
+            // 不鉴权
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        String uri = RequestContext.getCurrentContext().getRequest().getServletPath();
+        logger.info("请求地址" + uri);
+        return uri;
+    }
+}
---
+++ zuul/src/test/java/cc/wangweiye/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.wangweiye.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
