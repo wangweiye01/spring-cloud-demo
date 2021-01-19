64d73ce41ad57d251b54fb58194f19cf0dd1ecaf
wangweiye <wwyknight@163.com>
2021-01-19 14:13:38 +0800

    fix

--- consumer/src/main/java/cc/wangweiye/consumer/IndexController.java
+++ consumer/src/main/java/cc/wangweiye/consumer/IndexController.java
@@ -1,12 +1,14 @@
 package cc.wangweiye.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+    @Qualifier("cc.wangweiye.consumer.HelloFeignService")
     @Autowired
     private HelloFeignService feignService;
 
