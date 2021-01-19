adcb8ee5de2a468ebd5a369b82e5446b95b1a6bf
wangweiye <wwyknight@163.com>
2021-01-19 09:42:00 +0800

    fix

--- provider-service/src/main/java/cc/wangweiye/providerservice/HelloController.java
+++ provider-service/src/main/java/cc/wangweiye/providerservice/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class HelloController {
     @Value("${server.port}")
@@ -11,7 +13,9 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(String name) throws InterruptedException {
-        //Thread.sleep(60 * 1000);
+        long sleepTime = new Random().nextInt(15) * 1000;
+        System.out.println(String.format("睡眠时间：%s", sleepTime));
+        Thread.sleep(sleepTime);
 
         return port + ": hello " + name;
     }
