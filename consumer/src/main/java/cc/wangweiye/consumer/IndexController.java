6e695ef6608928e0eef321e9608eb1f6bf3e614b
wangweiye <wwyknight@163.com>
2021-01-22 15:50:48 +0800

    fix

--- consumer/src/main/java/cc/wangweiye/consumer/IndexController.java
+++ consumer/src/main/java/cc/wangweiye/consumer/IndexController.java
@@ -2,6 +2,7 @@ package cc.wangweiye.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,16 @@ public class IndexController {
     @Autowired
     private HelloFeignService feignService;
 
+    @Value("${a}")
+    private String aValue;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(String name) {
         return feignService.hello(name);
+    }
+
+    @RequestMapping(value = "/getConfig", method = RequestMethod.GET)
+    public String getConfig(String name) {
+        return aValue;
     }
 }
